e48112ed336c38a226323cb20ab40cb472df6851
Eduardo Henrique <eduardosatrini@gmail.com>
2021-03-13 17:42:16 -0300

    open exercises

---
+++ examples/types.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Go" + "lang!")         // strings
+	fmt.Println("5 + 5 =", 5+2)         // ints
+	fmt.Println("3.5 / 3.5 =", 3.5/3.5) // floats
+
+	fmt.Println(true && false) // booleans
+	fmt.Println(true || false) // booleans
+	fmt.Println(!false)        // booleans
+}
