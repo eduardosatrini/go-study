5b336be7bf3db6a1d6c40d433a25cd7e014bf1c5
Eduardo Henrique <eduardosatrini@gmail.com>
2021-04-18 21:35:48 -0300

    testes 1 and 2 OK

--- learning-go-with-tests/hello-name/hello_name.go
+++ learning-go-with-tests/hello-name/hello_name.go
@@ -1,5 +1,13 @@
 package hello_name
 
+const prefixPortuguese = "Olá, "
+const prefixEnglish = "Hello, "
+const prefixEspanish = "Hola, "
+
 func HelloName(name string) string {
-	return "Hello, " + name + "!"
+	if name != "" {
+		return "Hello, " + name + "!"
+	}
+
+	return "Hello, World!"
 }
--- learning-go-with-tests/hello-name/hello_name_test.go
+++ learning-go-with-tests/hello-name/hello_name_test.go
@@ -3,10 +3,26 @@ package hello_name
 import "testing"
 
 func TestHelloName(t *testing.T) {
-	result := HelloName("Eduardo")
-	expected := "Hello, Eduardo!"
 
-	if result != expected {
-		t.Errorf("result: '%s', expected: '%s'", result, expected)
+	verifyIfMessageIsCorretly := func(t *testing.T, result, expected string) {
+		t.Helper()
+		if result != expected {
+			t.Errorf("result: '%s', expected: '%s'", result, expected)
+		}
 	}
+
+	t.Run("Say hello for users", func(t *testing.T) {
+		result := HelloName("Eduardo")
+		expected := "Hello, Eduardo!"
+
+		verifyIfMessageIsCorretly(t, result, expected)
+	})
+
+	t.Run("say 'Hello World!' when string 'name' is a empty", func(t *testing.T) {
+		result := HelloName("")
+		expected := "Hello, World!"
+
+		verifyIfMessageIsCorretly(t, result, expected)
+	})
+
 }
