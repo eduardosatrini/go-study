edd67ed82ecb15aab0650494b6604533b15b0e97
Eduardo Henrique <eduardosatrini@gmail.com>
2021-04-28 21:44:56 -0300

    add tests with area interface

--- learning-go-with-tests/structures-methods-interfaces/geometry/geometry_test.go
+++ learning-go-with-tests/structures-methods-interfaces/geometry/geometry_test.go
@@ -13,29 +13,35 @@ func TestPerimeter(t *testing.T) {
 	if result != expected {
 		t.Errorf("result: %.2f, expected: %.2f", result, expected)
 	}
-
 }
 
 func TestArea(t *testing.T) {
 
-	t.Run("calc rectangler", func(t *testing.T) {
-		rectangle := Rectangle{5.0, 5.0}
-		result := rectangle.Area()
-		expected := 25.0
+	verifyArea := func(t *testing.T, shape Shape, expected float64) {
+
+		t.Helper()
+		result := shape.Area()
 
 		if result != expected {
 			t.Errorf("result: %.2f, expected: %.2f", result, expected)
 		}
+	}
+
+	t.Run("calc rectangler", func(t *testing.T) {
+
+		rectangle := Rectangle{5.0, 5.0}
+		verifyArea(t, rectangle, 25.0)
 	})
 
 	t.Run("calc circle", func(t *testing.T) {
-		circle := Circle{10}
-		result := circle.Area()
-		expected := 314.1592653589793
 
-		if result != expected {
-			t.Errorf("result: %.2f, expected: %.2f", result, expected)
-		}
+		circle := Circle{10}
+		verifyArea(t, circle, 314.1592653589793)
 	})
 
+	t.Run("calc triangle", func(t *testing.T) {
+
+		triangle := Triangle{12, 6}
+		verifyArea(t, triangle, 36.0)
+	})
 }
